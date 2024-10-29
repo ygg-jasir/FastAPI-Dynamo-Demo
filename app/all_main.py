e902887ccbdf5269cea9f992a92ca0881a3747a1
ygag-jasir <jasir@yougotagift.com>
2024-10-29 09:49:05 +0530

    access token and refresh token concept added

--- app/all_main.py
+++ app/all_main.py
@@ -2,17 +2,19 @@ from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi_users import FastAPIUsers, UUIDIDMixin
 from fastapi_users.manager import BaseUserManager
 from fastapi_users.authentication import JWTStrategy, AuthenticationBackend
-from fastapi_users.authentication import BearerTransport
+from fastapi_users.authentication import BearerTransport, CookieTransport
 from pydantic import BaseModel
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
 import uuid
-from typing import Optional
+from typing import List, Optional
 from fastapi_users import schemas
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import APIRouter
 from passlib.context import CryptContext
 from fastapi.concurrency import run_in_threadpool
+from fastapi.responses import JSONResponse
+
 # Set up logging
 import logging
 from decouple import config
@@ -55,10 +57,13 @@ class UserModel(Model):
     is_active = BooleanAttribute(default=True)
     is_superuser = BooleanAttribute(default=False)
     access_token = UnicodeAttribute(null=True)
+    refresh_token = UnicodeAttribute(null=True)
 
 # Pydantic models for user schema
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    pass
+    
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
 
 class UserCreate(schemas.BaseUserCreate):
     pass
@@ -92,17 +97,24 @@ class UserManager(UUIDIDMixin, BaseUserManager[UserModel, uuid.UUID]):
                 return user
         except UserModel.DoesNotExist:
             return None
+        
+    async def list_users(self) -> List[UserModel]:
+        result = UserModel.scan()
+        users = [user for user in result]
+        return users
 
     async def create(self, user: UserCreate, safe: bool = False, request: Optional[Request] = None) -> UserModel:
+        
         existing_user = await self.get_by_email(user.email)
+        
         if existing_user:
             raise HTTPException(status_code=400, detail="User with this email already exists.")
         user_db = UserModel(
             id=str(uuid.uuid4()),
             email=user.email,
             hashed_password=self.password_helper.hash(user.password),
-            is_active=True,
-            is_superuser=False,
+            is_active=user.is_active,
+            is_superuser=user.is_superuser,
             access_token = None,
         )
         user_db.save()
@@ -111,8 +123,22 @@ class UserManager(UUIDIDMixin, BaseUserManager[UserModel, uuid.UUID]):
 # JWT Authentication setup
 SECRET = "SECRET"
 
+# Constants for token lifetime
+ACCESS_TOKEN_LIFETIME = 3600  # 1 hour
+REFRESH_TOKEN_LIFETIME = 24 * 3600 * 7  # 7 days
+
+# Update JWT Strategy to support refresh token
+def get_access_token_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=ACCESS_TOKEN_LIFETIME)
+
+def get_refresh_token_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=REFRESH_TOKEN_LIFETIME)
+
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=ACCESS_TOKEN_LIFETIME)
+
+# Cookie transport for storing refresh token
+cookie_transport = CookieTransport(cookie_max_age=REFRESH_TOKEN_LIFETIME, cookie_name="refresh_token", cookie_httponly=True)
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
@@ -142,6 +168,10 @@ fastapi_users = FastAPIUsers[UserModel, uuid.UUID](
 # Custom Auth
 # Reuse the Bearer token authentication method
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/jwt/login")
+
+async def get_user_manager() -> UserManager:
+    return UserManager(UserModel)
+
 
 # Custom function to validate current user based on token
 async def JWTDBAuthentication(
@@ -181,7 +211,6 @@ async def private_route(current_user: UserModel = Depends(JWTDBAuthentication)):
     return {"message": f"Hello, {current_user.email}! This is a private route."}
 
 
-
 # Custom login endpoint
 @router.post("/auth/jwt/login", tags=["auth"])
 async def custom_login(
@@ -202,29 +231,100 @@ async def custom_login(
     if not await user_manager.validate_password(password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect email or password")
 
-    # Create JWT token
-    jwt_strategy = get_jwt_strategy()
-    token = await jwt_strategy.write_token(user)
+    # Create JWT access_token
+    # jwt_strategy = get_jwt_strategy()
+    jwt_strategy = get_access_token_strategy()
+    access_token = await jwt_strategy.write_token(user)
+    
+    # Create refresh token
+    refresh_token = await get_refresh_token_strategy().write_token(user)
 
     # Store the token in the user's record
-    user.access_token = token
-    user.update(actions=[UserModel.access_token.set(token)])
+    user.access_token = access_token
+    user.refresh_token = refresh_token
+    user.update(
+        actions=[
+            UserModel.access_token.set(access_token),
+            UserModel.refresh_token.set(refresh_token)
+            ]
+        )
+    response = JSONResponse({
+        "message": "Login Success", 
+        "user_id": user.id,
+        "access_token": access_token,
+        "refresh_token": refresh_token
+        })
+    
+    cookie_transport._set_login_cookie(response, refresh_token)
 
-    return {"access_token": token, "token_type": "bearer"}
-
+    return response
 
 
 # Custom logout endpoint
 @router.post("/auth/jwt/logout", tags=["auth"])
 async def logout(current_user: UserModel = Depends(fastapi_users.current_user())):
+    print("logout ")
     # Set access_token to None for the current user
     current_user.access_token = None
+    # Set refresh_token to None for the current user
+    current_user.refresh_token = None
 
     # Save the updated user model in PynamoDB
-    current_user.update(actions=[UserModel.access_token.remove()])
-
+    current_user.update(actions=[
+        UserModel.access_token.remove(),
+        UserModel.refresh_token.remove()
+        ])
+    response = JSONResponse({"message": "Logged out successfully"})
+    cookie_transport._delete_cookie(response)
     logger.info("User logged out: %s", current_user.email)
-    return {"message": "Logged out successfully"}
+    return response
+
+@router.post("/auth/jwt/refresh", tags=["auth"])
+async def refresh_token(request: Request, user_manager=Depends(get_user_manager)):
+    refresh_token = request.cookies.get("refresh_token")
+    print(refresh_token)
+    if not refresh_token:
+        raise HTTPException(status_code=401, detail="Refresh token not found")
+
+    jwt_strategy = get_refresh_token_strategy()
+    try:
+        # Decode and validate the refresh token
+        user_data = await jwt_strategy.read_token(refresh_token, user_manager=user_manager)
+        user = await user_manager.get(user_data.id)
+        if not user or user.refresh_token != refresh_token:
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+    # Generate new access token
+    new_access_token = await get_access_token_strategy().write_token(user)
+    user.access_token = new_access_token
+    user.update(actions=[UserModel.access_token.set(new_access_token)])
+
+    return {"token_type": "{}".format(new_access_token)}
+
+
+@router.get("/users/list", tags=["users"], response_model=List[UserRead])
+async def list_users_route(
+    current_user: UserModel = Depends(fastapi_users.current_user(active=True)),
+    user_manager: UserManager = Depends(get_user_manager),
+):
+    
+    # Ensure the current user has permission (e.g., is a superuser)
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="Insufficient permissions")
+
+    # Fetch all users
+    users = await user_manager.list_users()
+    return users
+
+@router.get("/users-list", tags=["users"],)
+async def list_users_route():
+    print("List usre")
+    # Fetch all users from the UserModel table
+    result = UserModel.scan()
+    users = [user for user in result]
+    return users
 
 # Include the routers
 app.include_router(router)
@@ -239,6 +339,8 @@ app.include_router(
     prefix="/users",
     tags=["users"]
 )
+
+
 
 @app.get("/")
 async def read_root():
